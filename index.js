b04d6c85d6fe74ff20ec05a4ca0ac6f34326e5ac
Vahe Sevachyan <vscode13@gmail.com>
2023-07-27 18:50:15 -0700

    updated all variable names

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-const laneModal = document.querySelector(".lane-modal");
-const addLaneBtn = document.querySelector(".add-lane-btn");
-const closeModalBtn = document.querySelector(".close-modal-btn");
-const saveModalBtn = document.querySelector(".save-modal-btn");
-const originInput = document.querySelector(".origin-input");
-const destinationInput = document.querySelector(".destination-input");
-const linkDisplay = document.querySelector(".link-display");
-function openLaneModal() {
-  laneModal.style.display = "block";
+const categoryModal = document.querySelector(".category-modal");
+const addCategoryBtn = document.querySelector(".add-category-btn");
+const closeCategoryBtn = document.querySelector(".close-category-btn");
+const saveCategoryBtn = document.querySelector(".save-category-btn");
+const urlNameInput = document.querySelector(".url-name-input");
+const categoryURLInput = document.querySelector(".category-url-input");
+const categoryNameContainer = document.querySelector(
+  ".category-name-container"
+);
+
+function openCategoryModal() {
+  categoryModal.style.display = "block";
 }
 
-function closeLaneModal() {
-  laneModal.style.display = "none";
+function closeCategoryModal() {
+  categoryModal.style.display = "none";
 }
 
-addLaneBtn.onclick = () => {
-  openLaneModal();
+addCategoryBtn.onclick = () => {
+  openCategoryModal();
 };
-closeModalBtn.onclick = () => {
-  closeLaneModal();
-};
-
-saveModalBtn.onclick = () => {
-  saveLaneModal();
-  closeLaneModal();
+closeCategoryBtn.onclick = () => {
+  closeCategoryModal();
 };
 
-function saveLaneModal() {
-  const laneDisplay = document.createElement("div");
-  laneDisplay.classList.add("laneDisplay");
-  const originInputValue = originInput.value;
-  laneDisplay.innerHTML = originInputValue;
-  linkDisplay.appendChild(laneDisplay);
+saveCategoryBtn.onclick = () => {
+  saveCategoryModal();
+  closeCategoryModal();
+};
+
+function saveCategoryModal() {
+  const urlNameContainer = document.createElement("div");
+  urlNameContainer.classList.add("categoryNameContainer");
+  let urlNameInputValue = urlNameInput.value;
+  urlNameContainer.innerHTML = urlNameInputValue;
+  categoryNameContainer.appendChild(urlNameContainer);
 }
